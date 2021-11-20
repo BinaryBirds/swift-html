e24ea159aeea65be80dffc3e3d5723e1d6653657
Tibor Bödecs <mail.tib@gmail.com>
2021-11-20 21:44:09 +0100

    html test

--- Tests/SwiftHtmlTests/SwiftHtmlTests.swift
+++ Tests/SwiftHtmlTests/SwiftHtmlTests.swift
@@ -11,44 +11,68 @@ import XCTest
 final class SwiftHtmlTests: XCTestCase {
         
     func testHtmlDocument() {
-//        let doc = Document {
-//            Html {
-//                Head {
-//                    Title("Hello Swift DSL")
-//                    
-//                    Meta().charset("utf-8")
-//                    Meta().name(.viewport).content("width=device-width, initial-scale=1")
-//
-//                    Link(rel: .stylesheet).href("./css/style.css")
-//                }
-//                Body {
-//                    Main {
-//                        Div {
-//                            Section {
-//                                Img(src: "./images/swift.png", alt: "Swift Logo")
-//                                    .title("Picture of the Swift Logo")
-//                                H1("Lorem ipsum")
-//                                    .class("red")
-//                                P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium leo eu euismod porta.")
-//                                    .class(["green", "blue"])
-//                                    .spellcheck(false)
-//                            }
-//                            A("Hello Swift HTML DSL!")
-//                                .href("https://swift.org")
-//                                .target(.blank)
-//                                .download()
-//                            Abbr("WHO")
-//                                .title("World Health Organization")
-//                        }
-//                    }
-//                    .class("container")
-//
-//                    Script()
-//                        .src("./javascript/main.js")
-//                }
-//            }
-//        }
-//        print(doc.html)
-//        XCTAssertTrue(true)
+        let doc = Document {
+            Html {
+                Head {
+                    Title("Hello Swift DSL")
+                    
+                    Meta().charset("utf-8")
+                    Meta().name(.viewport).content("width=device-width, initial-scale=1")
+
+                    Link(rel: .stylesheet).href("./css/style.css")
+                }
+                Body {
+                    Main {
+                        Div {
+                            Section {
+                                Img(src: "./images/swift.png", alt: "Swift Logo")
+                                    .title("Picture of the Swift Logo")
+                                H1("Lorem ipsum")
+                                    .class("red")
+                                P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium leo eu euismod porta.")
+                                    .class(["green", "blue"])
+                                    .spellcheck(false)
+                            }
+                            A("Hello Swift HTML DSL!")
+                                .href("https://swift.org")
+                                .target(.blank)
+                                .download()
+                            Abbr("WHO")
+                                .title("World Health Organization")
+                        }
+                    }
+                    .class("container")
+
+                    Script()
+                        .src("./javascript/main.js")
+                }
+            }
+        }
+
+        XCTAssertEqual(DocumentRenderer().render(doc), """
+                            <!DOCTYPE html>
+                            <html>
+                                <head>
+                                    <title>Hello Swift DSL</title>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                                    <link rel="stylesheet" href="./css/style.css">
+                                </head>
+                                <body>
+                                    <main class="container">
+                                        <div>
+                                            <section>
+                                                <img src="./images/swift.png" alt="./images/swift.png" title="Picture of the Swift Logo">
+                                                <h1 class="red">Lorem ipsum</h1>
+                                                <p class="green blue" spellcheck="false">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium leo eu euismod porta.</p>
+                                            </section>
+                                            <a href="https://swift.org" target="_blank" download>Hello Swift HTML DSL!</a>
+                                            <abbr title="World Health Organization">WHO</abbr>
+                                        </div>
+                                    </main>
+                                    <script src="./javascript/main.js"></script>
+                                </body>
+                            </html>
+                            """)
     }
 }
