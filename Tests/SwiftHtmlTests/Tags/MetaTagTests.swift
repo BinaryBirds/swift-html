2b5d815ba1cf27b7aaf9bae4c6891e66bcbc1dce
Tibor Bodecs <mail.tib@gmail.com>
2022-01-21 17:48:10 +0100

    meta tests

---
+++ Tests/SwiftHtmlTests/Tags/MetaTagTests.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tibor Bodecs on 2022. 01. 21..
+//
+
+import XCTest
+@testable import SwiftHtml
+
+final class MetaTagTests: XCTestCase {
+
+    func testColorScheme() {
+        let doc = Document {
+            Meta()
+                .name(.colorScheme)
+                .content("light dark")
+        }
+        let html = DocumentRenderer(minify: true).render(doc)
+        XCTAssertEqual(#"<meta name="color-scheme" content="light dark">"#, html)
+    }
+    
+    func testMediaColorScheme() {
+        let doc = Document {
+            Meta()
+                .name(.themeColor)
+                .content("#fff")
+                .mediaPrefersColorSchemeLight()
+        }
+        let html = DocumentRenderer(minify: true).render(doc)
+        XCTAssertEqual(##"<meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)">"##, html)
+    }
+    
+    func testAppleStatusBarStyle() {
+        let doc = Document {
+            Meta()
+                .name(.appleMobileWebAppStatusBarStyle)
+                .content("default")
+        }
+        let html = DocumentRenderer(minify: true).render(doc)
+        XCTAssertEqual(##"<meta name="apple-mobile-web-app-status-bar-style" content="default">"##, html)
+    }
+
+}
