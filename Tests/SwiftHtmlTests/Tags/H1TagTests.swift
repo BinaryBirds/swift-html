2201405802f3012180b4e1263d02c5dad36c0ef7
Tibor Bodecs <mail.tib@gmail.com>
2022-02-11 14:55:24 +0100

    h1 tests

---
+++ Tests/SwiftHtmlTests/Tags/H1TagTests.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tibor Bodecs on 2022. 02. 11..
+//
+
+import XCTest
+@testable import SwiftHtml
+
+final class H1TagTests: XCTestCase {
+
+    func testInitWithContents() {
+        let doc = Document {
+            H1("Lorem ipsum")
+        }
+        XCTAssertEqual(DocumentRenderer(minify: true).render(doc), #"<h1>Lorem ipsum</h1>"#)
+    }
+    
+    func testInitWithTag() {
+        let doc = Document {
+            H1 {
+                A("foo")
+                    .id("bar")
+            }
+        }
+        XCTAssertEqual(DocumentRenderer(minify: true).render(doc), #"<h1><a id="bar">foo</a></h1>"#)
+    }
+    
+    func testInitWithEmptyContents() {
+        let doc = Document {
+            H1()
+                .setContents("foo")
+        }
+        XCTAssertEqual(DocumentRenderer(minify: true).render(doc), #"<h1>foo</h1>"#)
+    }
+    
+    func testInitWithChildren() {
+        let doc = Document {
+            H1([
+                A("foo")
+            ])
+        }
+        XCTAssertEqual(DocumentRenderer(minify: true).render(doc), #"<h1><a>foo</a></h1>"#)
+    }
+
+}
