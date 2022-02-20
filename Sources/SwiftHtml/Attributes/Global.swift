2c17e64e16ea56c7a795374eba70b9f732f02515
bgisme <55024704+bgisme@users.noreply.github.com>
2022-02-20 17:30:40 -0500

    Update Global.swift

--- Sources/SwiftHtml/Attributes/Global.swift
+++ Sources/SwiftHtml/Attributes/Global.swift
@@ -174,7 +174,7 @@ public extension Tag {
     /// Specifies an inline CSS style for an element
     func style(_ value: String) -> Self {
         guard !value.isEmpty else { return self }
-        attribute("style", value)
+        return attribute("style", value)
     }
     
     /// Specifies the tabbing order of an element
