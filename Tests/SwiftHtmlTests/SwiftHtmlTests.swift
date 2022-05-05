ac5343b1664846f2def21f5900a5a4a7c7d11c26
bgisme <55024704+bgisme@users.noreply.github.com>
2022-05-05 15:36:34 -0400

    Update SwiftHtmlTests.swift

--- Tests/SwiftHtmlTests/SwiftHtmlTests.swift
+++ Tests/SwiftHtmlTests/SwiftHtmlTests.swift
@@ -53,8 +53,8 @@ final class SwiftHtmlTests: XCTestCase {
         let doc = Document {
             Span("")
                 .class("a", "b", "c")
-                .class(add: ["d", "e", "f"])
-                .class(add: "b", true)
+                .class(insert: ["d", "e", "f"])
+                .class(insert: "b", true)
                 .class(remove: ["b", "c", "d"])
                 .class(remove: "e", true)
         }
@@ -66,7 +66,7 @@ final class SwiftHtmlTests: XCTestCase {
         let doc = Document {
             Span("")
                 .class("a", "b", "c")
-                .class(add: "d")
+                .class(insert: "d")
         }
         let html = DocumentRenderer(minify: true).render(doc)
         XCTAssertEqual(#"<span class="a b c d"></span>"#, html)
