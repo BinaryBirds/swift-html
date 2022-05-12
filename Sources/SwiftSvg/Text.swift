76677755ae99255a2633f7523a6d2984175aa14c
AaronGe88inTHU <37819541+AaronGe88inTHU@users.noreply.github.com>
2022-05-12 16:22:14 +0800

    Update Text.swift

--- Sources/SwiftSvg/Text.swift
+++ Sources/SwiftSvg/Text.swift
@@ -23,22 +23,3 @@ public extension Text{
     }
     
 }
-
-
-//open class Line: Tag {
-//
-//    public init(x1: Double,
-//                y1: Double,
-//                x2: Double,
-//                y2: Double,
-//                pathLength: Double? = nil) {
-//        super.init()
-//        setAttributes([
-//            .init(key: "x1", value: x1.preciseString),
-//            .init(key: "y1", value: y1.preciseString),
-//            .init(key: "x2", value: x2.preciseString),
-//            .init(key: "y2", value: y2.preciseString),
-//        ])
-//        attribute("pathLength", pathLength?.preciseString)
-//    }
-//}
