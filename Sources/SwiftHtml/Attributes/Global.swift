49e52caed9faeb1ef6946c37008f0c108b73edea
atacan <atacandur@icloud.com>
2023-07-03 16:28:22 +0200

    Adds inline style management (#31)
    * style methods adapted from class
    * remove style by key property name
    * remove previous style method

--- Sources/SwiftHtml/Attributes/Global.swift
+++ Sources/SwiftHtml/Attributes/Global.swift
@@ -37,12 +37,21 @@ private extension String {
     var classArray: [String] {
         split(separator: " ").map { String($0) }
     }
+    
+    /// turns a list of style values (separated by a semicolon) into an array of strings
+    var styleArray: [String] {
+        split(separator: ";").map { String($0) }
+    }
 }
 
 private extension Array where Element == String {
     
     var classString: String {
         filter { !$0.isEmpty }.joined(separator: " ")
+    }
+    
+    var styleString: String {
+        filter { !$0.isEmpty }.joined(separator: ";")
     }
 }
 
@@ -123,6 +132,104 @@ public extension Tag {
         }
         return `class`(add: value, condition)
     }
+
+    // MARK: - style management
+
+    /// find an existing style attribute and return the value as an array of strings or an empty array
+    private var styleArray: [String] {
+        node.attributes.first { $0.key == "style" }?.value?.styleArray ?? []
+    }
+
+    /// Specifies one stylename for an element (refers to a style in a style sheet)
+    func style(_ value: String?, _ condition: Bool = true) -> Self {
+        guard let value, !value.isEmpty else { return self }
+        return attribute("style", value, condition)
+    }
+
+    /// Specifies multiple stylenames for an element (refers to a style in a style sheet)
+    func style(_ values: [String], _ condition: Bool = true) -> Self {
+        /// @NOTE: explicit true flag is needed, otherwise Swift won't know which function to call...
+        style(values.styleString, condition)
+    }
+
+    /// Specifies multiple stylenames for an element (refers to a style in a style sheet)
+    func style(_ values: String...) -> Self {
+        style(values)
+    }
+    
+    /// Adds a single value to the style list if the condition is true
+    ///
+    /// Note: If the value is empty or nil it won't be added to the list
+    ///
+    func style(add value: String?, _ condition: Bool = true) -> Self {
+        guard let value = value else {
+            return self
+        }
+        return style(add: [value], condition)
+    }
+    
+    /// Adds an array of values to the style list if the condition is true
+    ///
+    /// Note: If the value is empty it won't be added to the list
+    ///
+    func style(add values: [String], _ condition: Bool = true) -> Self {
+        let newValues = styleArray + values.filter { !$0.isEmpty }
+
+        var newValue: String? = nil
+        if !newValues.isEmpty {
+            newValue = newValues.styleString
+        }
+        return style(newValue, condition)
+    }
+    
+    /// Removes a given style values if the condition is true
+    func style(remove value: String?, _ condition: Bool = true) -> Self {
+        guard let value = value else {
+            return self
+        }
+        return style(remove: [value], condition)
+    }
+    
+    /// Removes an array of style values if the condition is true
+    func style(remove values: [String], _ condition: Bool = true) -> Self {
+        let newClasses = styleArray.filter { !values.contains($0) }
+        if newClasses.isEmpty {
+            return deleteAttribute("style")
+        }
+        return style(newClasses, condition)
+    }
+    
+    /// Removes a given style value with its key name if the condition is true
+    /// `.style(removeByKey: "font-size")` as opposed to `.style(remove: "font-size: 12rem")`
+    func style(removeByKey value: String?, _ condition: Bool = true) -> Self {
+        guard let value = value else {
+            return self
+        }
+        return style(removeByKey: [value], condition)
+    }
+
+    
+    /// Removes an array of style values with the key name if the condition is true
+    /// `.style(removeByKey:[ "font-size"])` as opposed to `.style(remove: ["font-size: 12rem"])`
+    func style(removeByKey values: [String], _ condition: Bool = true) -> Self {
+        let newClasses = styleArray.filter { !values.contains(String($0.prefix(while: {$0 != ":"}))) }
+        if newClasses.isEmpty {
+            return deleteAttribute("style")
+        }
+        return style(newClasses, condition)
+    }
+    
+    /// toggles a single style value
+    func style(toggle value: String?, _ condition: Bool = true) -> Self {
+        guard let value = value else {
+            return self
+        }
+        if styleArray.contains(value) {
+            return style(remove: value, condition)
+        }
+        return style(add: value, condition)
+    }
+
     
     // MARK: - other global attributes
     
@@ -169,12 +276,6 @@ public extension Tag {
     /// Specifies whether the element is to have its spelling and grammar checked or not
     func spellcheck(_ value: Bool) -> Self {
         attribute("spellcheck", String(value))
-    }
-    
-    /// Specifies an inline CSS style for an element
-    func style(_ value: String) -> Self {
-        guard !value.isEmpty else { return self }
-        return attribute("style", value)
     }
     
     /// Specifies the tabbing order of an element
