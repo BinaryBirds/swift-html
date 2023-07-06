cb0eada6cc1db9eb78f9047157af408f00d616e0
bgisme <55024704+bgisme@users.noreply.github.com>
2023-07-06 18:18:11 -0400

    Reverting to init with optional Node

--- Sources/SwiftSgml/TagBuilder.swift
+++ Sources/SwiftSgml/TagBuilder.swift
@@ -8,26 +8,30 @@
 @resultBuilder
 public enum TagBuilder {
     
+    public static func buildBlock(_ components: [Tag]...) -> [Tag] {
+        components.flatMap { $0 }
+    }
+    
     public static func buildBlock(_ components: [Tag]...) -> Tag {
         GroupTag(components.flatMap { $0 })
     }
-
+    
     public static func buildBlock(_ components: Tag...) -> Tag {
         GroupTag(components)
     }
-
+    
     public static func buildOptional(_ component: Tag?) -> Tag {
         component ?? GroupTag()
     }
-
+    
     public static func buildEither(first component: Tag) -> Tag {
         component
     }
-
+    
     public static func buildEither(second component: Tag) -> Tag {
         component
     }
-
+    
     public static func buildArray(_ components: [Tag]) -> Tag {
         GroupTag(components)
     }
