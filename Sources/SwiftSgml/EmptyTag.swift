677973dbf268cfec2ad28ff3ee830000f735be5a
bgisme <55024704+bgisme@users.noreply.github.com>
2023-07-06 17:24:41 -0400

    Reverting to init with optional Node

--- Sources/SwiftSgml/EmptyTag.swift
+++ Sources/SwiftSgml/EmptyTag.swift
@@ -1,14 +1,16 @@
 //
-//  File.swift
+//  EmptyTag.swift
 //  
 //
 //  Created by Tibor Bodecs on 2022. 02. 11..
 //
 
 open class EmptyTag: Tag {
-
-//    open class override var type: Node.NodeType { .empty }
     
+    open override class func createNode() -> Node {
+        Node(type: .empty, name: String(describing: self).lowercased())
+    }
+
     public init() {
         super.init()
     }
--- Sources/SwiftSgml/GroupTag.swift
+++ Sources/SwiftSgml/GroupTag.swift
@@ -1,9 +1,14 @@
 //
-//  File.swift
+//  GroupTag.swift
 //  
 //
 //  Created by Tibor Bodecs on 2022. 02. 11..
 //
 
 
-open class GroupTag: Tag {}
+open class GroupTag: Tag {
+
+    open override class func createNode() -> Node {
+        .init(type: .group)
+    }
+}
