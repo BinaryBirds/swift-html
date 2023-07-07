a897e29c4e0086dca12ba01445f5fb151ced6a0f
bgisme <55024704+bgisme@users.noreply.github.com>
2023-07-07 10:46:27 -0400

    init Tag with optional Node

--- Sources/SwiftSgml/Tag.swift
+++ Sources/SwiftSgml/Tag.swift
@@ -23,13 +23,13 @@ open class Tag {
     }
 
     /// initialize a new Tag with a single child tag
-    public convenience init(_ child: Tag) {
-        self.init([child])
+    public convenience init(node: Node? = nil, _ child: Tag) {
+        self.init(node: node, [child])
     }
 
     /// initialize a new Tag with children using a builder
-    public convenience init(@TagBuilder _ builder: () -> Tag) {
-        self.init([builder()])
+    public convenience init(node: Node? = nil, @TagBuilder _ builder: () -> Tag) {
+        self.init(node: node, [builder()])
     }
     
     /// initialize a new Tag with some contents
