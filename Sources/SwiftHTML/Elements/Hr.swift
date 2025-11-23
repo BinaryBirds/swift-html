9babccbbd35b74a3d651350afc2c66df88a62526
Tibor Bodecs <mail.tib@gmail.com>
2025-11-23 11:09:03 +0100

    fix format

--- Sources/SwiftHTML/Elements/Hr.swift
+++ Sources/SwiftHTML/Elements/Hr.swift
@@ -1,6 +1,6 @@
 import SGML
 
-/// The `<hr>` tag defines a thematic break in an HTML page (e.g. a shift of topic).
+/// The `<hr>` tag defines a thematic break in an HTML page (a shift of topic).
 ///
 /// The `<hr>` element is most often displayed as a horizontal rule that is used to separate content (or define a change) in an HTML page.
 public struct Hr: ShortTag {
